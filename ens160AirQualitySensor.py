b36009cdba16158b70aac56f94f055f1a0e7124c
Jordan Auld <jordauld1@gmail.com>
2024-01-02 22:39:20 +1000

    Adding influxdb database logging

--- ens160AirQualitySensor.py
+++ ens160AirQualitySensor.py
@@ -2,6 +2,7 @@
 # Shows three metrics: AQI, TVOC and eCO2
 
 import math
+import logging
 from PiicoDev_SSD1306 import *  # import the OLED device driver
 from PiicoDev_ENS160 import PiicoDev_ENS160  # import the device driver
 from PiicoDev_BME280 import (
@@ -10,14 +11,53 @@ from PiicoDev_BME280 import (
 from PiicoDev_TMP117 import PiicoDev_TMP117  # import TMP117 device driver
 from PiicoDev_Unified import sleep_ms  # a cross-platform sleep function
 
+import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "raider"
+url = "http://192.168.1.10:8086"
+bucket = "sensorData"
+
+# Initialize InfluxDB Client
+write_client = InfluxDBClient(url=url, token=token, org=org)
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
 
 # Initialize sensors and display
 def init_devices():
     temp_sensor = PiicoDev_TMP117()  # initialise the precision temperature sensor
     air_quality_sensor = PiicoDev_ENS160()  # Initialise the ENS160 air quality sensor
     atmospheric_sensor = PiicoDev_BME280()  # initialise the atmospheric sensor
-    display = create_PiicoDev_SSD1306()      # initialise the OLED display driver
+    display = create_PiicoDev_SSD1306()  # initialise the OLED display driver
     return temp_sensor, air_quality_sensor, atmospheric_sensor, display
+
+
+def write_to_influx(temperature, humidity, pres_hPa, aqi, aqiRating, tvoc, eco2, eco2Rating, sensorStatus):
+    try:
+        point = (
+            Point("sensorReading")
+            .tag("sensor", "PiicoDevSensors")
+            .tag("location", "bedroom3")
+            .field("temperature", temperature)
+            .field("humidity", humidity)
+            .field("pressure", pres_hPa)
+            .field("aqi", aqi)
+            .field("tvoc", tvoc)
+            .field("eco2", eco2)
+            .field("aqi_rating", aqiRating)
+            .field("eco2_rating", eco2Rating)
+            .field("sensor_status", sensorStatus)
+        )
+        write_api.write(bucket=bucket, org=org, record=point)
+    except Exception as e:
+        logging.error("Error writing to InfluxDB: %s", e)
 
 
 # Air Quality signal characteristics
@@ -77,14 +117,26 @@ def update_display(display, sensor_data, page=0):
         display.text(f"Temp: {sensor_data['tempC']}C", 0, 0, 1)
         display.text(f"Humid: {sensor_data['humRH']}%", 0, 10, 1)
         display.text(f"Press: {sensor_data['pres_hPa']}hPa", 0, 20, 1)
-        display.text(f"AQI: {sensor_data['aqi']}" + " [" + str(sensor_data['aqi_rating']) + "]", 0, 30, 1)
+        display.text(
+            f"AQI: {sensor_data['aqi']}" + " [" + str(sensor_data["aqi_rating"]) + "]",
+            0,
+            30,
+            1,
+        )
         display.text(f"TVOC: {sensor_data['tvoc']}ppb", 0, 40, 1)
-        display.text(f"eCO2: {sensor_data['eco2']}ppm" + " [" + str(sensor_data['eco2_rating']) + "]", 0, 50, 1)
-        
+        display.text(
+            f"eCO2: {sensor_data['eco2']}ppm"
+            + " ["
+            + str(sensor_data["eco2_rating"])
+            + "]",
+            0,
+            50,
+            1,
+        )
 
     # elif page == 1:
-        # Third page: Other metrics or messages
-        #display.text(f"Sensor Status: {sensor_data['sensor_status']}", 0, 0, 1)
+    # Third page: Other metrics or messages
+    # display.text(f"Sensor Status: {sensor_data['sensor_status']}", 0, 0, 1)
 
     display.show()
 
@@ -100,7 +152,20 @@ def main():
             )
             update_console(sensor_data)
             update_display(display, sensor_data, page)
-            #page = (page + 1) % 2  # Cycle through 2 pages
+
+            write_to_influx(
+                sensor_data["tempC"],
+                sensor_data["humRH"],
+                sensor_data["pres_hPa"],
+                sensor_data["aqi"],
+                sensor_data["aqi_rating"],
+                sensor_data["tvoc"],
+                sensor_data["eco2"],
+                sensor_data["eco2_rating"],
+                sensor_data["sensor_status"],
+            )
+
+            # page = (page + 1) % 2  # Cycle through 2 pages
             sleep_ms(1000)
         except Exception as e:
             print(f"Error: {e}")
--- humidity_logger.py
+++ humidity_logger.py
@@ -4,35 +4,76 @@ import os
 import time
 import Adafruit_DHT
 import logging
+import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "raider"
+url = "http://192.168.1.10:8086"
+bucket = "sensorData"
+
+print("Token: ", token)  # Remove this line after verification
+
+# Initialize InfluxDB Client
+write_client = InfluxDBClient(url=url, token=token, org=org)
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
-FILENAME = '/home/pi/humidity.csv'
+FILENAME = "/home/pi/humidity.csv"
+
 
 def initialize_file():
     if not os.path.exists(FILENAME) or os.stat(FILENAME).st_size == 0:
-        with open(FILENAME, 'w') as f:
-            f.write('Date,Time,Temperature,Humidity\r\n')
+        with open(FILENAME, "w") as f:
+            f.write("Date,Time,Temperature,Humidity\r\n")
+
+
+def write_to_influx(temperature, humidity):
+    try:
+        point = (
+            Point("sensorReading")
+            .tag("sensor", "DHT22")
+            .tag("location", "bedroom3")
+            .field("temperature", temperature)
+            .field("humidity", humidity)
+        )
+        write_api.write(bucket=bucket, org=org, record=point)
+    except Exception as e:
+        logging.error("Error writing to InfluxDB: %s", e)
+
 
 def log_sensor_data():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        logging.info("Temp: %s, Humidity: %s", temperature, humidity)
-        print("Temp: %s, Humidity: %s", temperature, humidity)
-        with open(FILENAME, 'a') as f:
-            f.write('{0},{1},{2:0.1f},{3:0.1f}%\r\n'.format(time.strftime('%d/%m/%y'), time.strftime('%H:%M'), temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor.")
-        logging.error("Failed to retrieve data from humidity sensor")
+    try:
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            logging.info("Temp: %s, Humidity: %s", temperature, humidity)
+            with open(FILENAME, "a") as f:
+                f.write(
+                    "{0},{1},{2:0.1f},{3:0.1f}%\r\n".format(
+                        time.strftime("%d/%m/%y"),
+                        time.strftime("%H:%M"),
+                        temperature,
+                        humidity,
+                    )
+                )
+            write_to_influx(temperature, humidity)
+    except Exception as e:
+        logging.error("Error occurred: %s", e)
+
 
 def main():
     initialize_file()
     while True:
         log_sensor_data()
         time.sleep(15)
+
 
 if __name__ == "__main__":
     main()
